786be9c1a42fc5fc854837871386029d51cb79e5
Dominggus Octovianus <dominggus.octovianus@gmail.com>
2021-12-04 17:19:55 +0700

    Temp

--- pdfViewer/src/main/java/com/rajat/pdfviewer/util/FileUtils.kt
+++ pdfViewer/src/main/java/com/rajat/pdfviewer/util/FileUtils.kt
@@ -1,16 +1,13 @@
 package com.rajat.pdfviewer.util
 
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Environment
-import android.provider.MediaStore
-import android.text.TextUtils
+import android.util.Log
+import android.widget.Toast
 import java.io.*
-import android.util.Log;
-import java.io.PrintWriter
-import java.io.StringWriter
-import android.content.Intent;
-import android.widget.Toast;
-//import android.net.Uri;
+
 //import android.app.DownloadManager
 
 object FileUtils {
@@ -55,15 +52,19 @@ object FileUtils {
 
             var ins: InputStream = localPdf.inputStream()
             copy(ins, outFile1)
-            //val uri = Uri.fromFile(outFile1)
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.setDataAndType(Uri.parse(outFile1.toString()), "file/*")
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK;
             val toast = Toast.makeText(context, "Successfully Save PDF To Download", 3000)
             toast.show()
-            //context.startActivity(Intent(DownloadManager.ACTION_VIEW_DOWNLOADS));
+            context.startActivity(intent);
         } catch(e: Exception) {
             val sw = StringWriter()
             e.printStackTrace(PrintWriter(sw))
             val exceptionAsString = sw.toString()
             Log.d(">>>>>", exceptionAsString);
+            val toast = Toast.makeText(context, "Failed Save PDF To Download: /${exceptionAsString}", 5000)
+            toast.show()
         }
     }
 }
