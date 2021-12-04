1b112347180d9b463331db64e15909192d593a75
Dominggus Octovianus <dominggus.octovianus@gmail.com>
2021-12-04 20:03:18 +0700

    Temp 2

--- pdfViewer/src/main/java/com/rajat/pdfviewer/util/FileUtils.kt
+++ pdfViewer/src/main/java/com/rajat/pdfviewer/util/FileUtils.kt
@@ -3,10 +3,15 @@ package com.rajat.pdfviewer.util
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import android.os.Environment
+import android.provider.DocumentsContract
 import android.util.Log
 import android.widget.Toast
+import androidx.core.content.FileProvider
+import com.rajat.pdfviewer.BuildConfig
 import java.io.*
+
 
 //import android.app.DownloadManager
 
@@ -49,21 +54,36 @@ object FileUtils {
             }
             val outFile1 = File(dirPath, "/$fileName.pdf")
             val localPdf = File(assetName)
-
             var ins: InputStream = localPdf.inputStream()
             copy(ins, outFile1)
+
+            val myDir = Uri.parse(dirPath)
             val intent = Intent(Intent.ACTION_VIEW)
-            intent.setDataAndType(Uri.parse(dirPath), "application/pdf")
-            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP;
-            val toast = Toast.makeText(context, "Successfully Save PDF To Download", 3000)
-            toast.show()
-            context.startActivity(intent);
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)
+                intent.setDataAndType(myDir,  DocumentsContract.Document.MIME_TYPE_DIR)
+            else  intent.setDataAndType(myDir,  "application/pdf")
+
+            if (intent.resolveActivityInfo(context.packageManager, 0) != null)
+            {
+                context.startActivity(intent)
+                val toast = Toast.makeText(context, "Successfully Save PDF To Download", 5000)
+                toast.show()
+            }
+            else
+            {
+                // if you reach this place, it means there is no any file
+                // explorer app installed on your device
+                val toast = Toast.makeText(context, "Failed Save PDF To Download", Toast.LENGTH_LONG)
+                toast.show()
+            }
         } catch(e: Exception) {
             val sw = StringWriter()
             e.printStackTrace(PrintWriter(sw))
             val exceptionAsString = sw.toString()
             Log.d(">>>>>", exceptionAsString);
-            val toast = Toast.makeText(context, "Failed Save PDF To Download: /${exceptionAsString}", 5000)
+            val toast = Toast.makeText(context, "Failed Save PDF To Download: /${exceptionAsString}", Toast.LENGTH_LONG)
             toast.show()
         }
     }
